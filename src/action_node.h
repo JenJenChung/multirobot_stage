21896975c13f30af7463f2ebb57951dd330c474a
Julian von der Goltz <j.vdgoltz@gmail.com>
2018-04-18 13:04:09 +0200

    correct initialization of ROSparams, add action publisher

--- src/action_node.h
+++ src/action_node.h
@@ -14,6 +14,14 @@ Published topics:
 Services:
 - check if waypoint reached succesfully
 
+Logic:
+    - subscribe to local shared map
+    - subscribe to odometries
+    - while not finished:
+        - calculate current_state_ according to map and last known odometries
+        - if ready_flag:
+            - getAction(current_state_)
+            - ready_flag=False # the ready flag will be set to True if the robot has reached the waypoint, conducted measurements and exchanged information
 */
 # include <ros/ros.h>
 # include <math.h>
@@ -24,6 +32,7 @@ Services:
 # include <geometry_msgs/Pose2D.h>
 # include <geometry_msgs/Twist.h>
 # include <Eigen/Dense>
+# include <conversions.h>
 
 class ActionNode{
     public:
@@ -49,6 +58,7 @@ class ActionNode{
         void odomCallback(const nav_msgs::Odometry::ConstPtr&, nav_msgs::Odometry*);
 
         geometry_msgs::Twist getAction();
+        geometry_msgs::Twist getAction(const Eigen::MatrixXd&);
 };
 
 ActionNode::ActionNode(ros::NodeHandle n){
@@ -59,16 +69,24 @@ ActionNode::ActionNode(ros::NodeHandle n){
     */
     nh_ = n;
 
-    // Initialize params:
-    robot_id_ = 0; // nh_.getParam();
+    // Initialize ROS params:
+    nh_.param<int>("robot_id", robot_id_, 0);
+    nh_.param<int>("nRobots", nRobots_, 1);
 
-    std::string merged_map_topic = "map"; // = nh_.getParam();
-    std::string odom_topic  = "odom";
-    std::string rootns = "robot"; // = nh_.getParam();
-    nRobots_ = 2; // = nh_.getParam();
+    std::string rootns, merged_map_topic, odom_topic, action_topic; 
+    nh_.param<std::string>("root_namespace", rootns, "robot");
+    nh_.param<std::string>("merged_map_topic", merged_map_topic, "map");
+    nh_.param<std::string>("odom_topic", odom_topic, "odom");
+    nh_.param<std::string>("action_topic", action_topic, "action");
 
     odoms_ = new nav_msgs::Odometry[nRobots_];
     odom_sub_ = new ros::Subscriber[nRobots_];
+
+    // Load policy
+    /* load the policy, possible formats:
+        - weights or other parameters of a predefined function
+        - a generic function defined in a separate file or script:
+    */
 
     // Initialize Subscribers and Publishers
     std::stringstream mergedmaptopic;
@@ -82,17 +100,31 @@ ActionNode::ActionNode(ros::NodeHandle n){
         odom_sub_[r] = nh_.subscribe<nav_msgs::Odometry>(robotopic.str(), 10, boost::bind(&ActionNode::odomCallback, this, _1, &odoms_[r]));
         ROS_INFO_STREAM("Robot " << robot_id_ << ": Subscribed to: " << robotopic.str());
     }
+    std::stringstream actiontopic;
+    actiontopic << rootns << "_" << robot_id_ << "/" << action_topic;
+    action_pub_ = nh_.advertise<geometry_msgs::Twist>(actiontopic.str(),10);
 }
     
-
 void ActionNode::mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg){
     merged_map_ = *msg;
+    // TODO: this callback will also calculate the current_state
 }
 
 void ActionNode::odomCallback(const nav_msgs::Odometry::ConstPtr& msg_in, nav_msgs::Odometry *msg_out)
 {
-  *msg_out = *msg_in;
+    *msg_out = *msg_in;
 }
 
 geometry_msgs::Twist ActionNode::getAction(){
+    // This method outputs an action belonging to a state according to the loaded policy
+    geometry_msgs::Twist action = ActionNode::getAction(current_state_);
+    action_pub_.publish<geometry_msgs::Twist>(action);
+    return action;
+}
+
+geometry_msgs::Twist ActionNode::getAction(const Eigen::MatrixXd &state){
+    // This method outputs an action belonging to a state according to the loaded policy
+    geometry_msgs::Twist action; 
+    // TODO: insert policy here
+    return action;
 }
