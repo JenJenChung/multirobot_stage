4a6536cedab5082fd632372e86a26ea840750560
Benjamin Hahn <hahnb@student.ethz.ch>
2018-06-01 11:19:04 +0200

    added logging for PG

--- src/multiRobotPG.h
+++ src/multiRobotPG.h
@@ -7,6 +7,8 @@
 #include <Eigen/Eigen>
 #include <float.h>
 
+#include <boost/filesystem.hpp>
+
 #include "multirobot_stage/PolicyGrad.h"
 #include "multirobot_stage/NeuralNet.h"
 
@@ -34,11 +36,15 @@ class MultiRobotPG{
     vector<double> rewardLog ;
     //double maxR ;
     //int maxTeam ;
+
+    boost::filesystem::path log_dir;
+    std::string rewards_file_name = "rewards.csv";
     
     ros::Subscriber subResult ;
     void episodeCallback(const std_msgs::Float64&) ;
     
     void NewEpisode() ;
+    void writeRewardsToFile(double reward);
 };
 
 MultiRobotPG::MultiRobotPG(ros::NodeHandle nh){
@@ -73,13 +79,36 @@ MultiRobotPG::MultiRobotPG(ros::NodeHandle nh){
   // Subscriber
   subResult = nh.subscribe("/episode_result", 10, &MultiRobotPG::episodeCallback, this) ;
   
+  auto ct = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::stringstream cur_time_and_date;
+  cur_time_and_date << std::put_time(std::localtime(&ct), "%F %T");
+  log_dir = boost::filesystem::path("logs" + cur_time_and_date.str());
+  boost::filesystem::create_directory(log_dir);
+  ros::param::set("/learning/log_dir", log_dir.string());
+
+  // log results
+  std::ofstream rewards_file;
+  rewards_file.open(log_dir.string() + "/" + rewards_file_name, std::ios_base::app);
+  rewards_file << "episode_reward,episode_num" << std::endl;
+
   NewEpisode() ;
+}
+
+void MultiRobotPG::writeRewardsToFile(double reward) {
+  std::ofstream rewards_file;
+  rewards_file.open(log_dir.string() + "/" + rewards_file_name, std::ios_base::app);
+  rewards_file << std::to_string(reward)  << "," << 
+        std::to_string(epochCount) << std::endl;
 }
 
 void MultiRobotPG::episodeCallback(const std_msgs::Float64& msg){
   // Read out reward
   double r = msg.data ;
   rewardLog.push_back(r) ; 
+
+    // write rewards to file
+  writeRewardsToFile(r);
+
   NewEpisode() ;
 }
 
